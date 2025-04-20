c415507f0ee7dda46946d6f15336b59a4d07c75b
AbdullahKus <abdullahkus080@gmail.com>
2025-04-20 20:35:07 +0300

    Add project files.

---
+++ AcunMedyaTravelProject/Context/Context.cs
@@ -0,0 +1,19 @@
+﻿using AcunMedyaTravelProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+namespace AcunMedyaTravelProject.Context
+{
+    public class Context:DbContext
+    { 
+        //tablonun adını(sınıfın adını) car -> cars
+        public DbSet<Category> Categories { get;set;}
+        public DbSet<Testimonial> Testimonials {get;set;}
+        public DbSet<Destinations> Destinations {get;set;}
+        public DbSet<Slider> Sliders {get;set;}
+        public DbSet<Services> Services {get;set;}
+
+    }
+}
---
+++ AcunMedyaTravelProject/Entities/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AcunMedyaTravelProject.Entities
+{
+    public class Category
+    {
+        //categoryıd, categoryname, 
+        public int CategoryID { get; set; }
+
+        public string CategoryName { get; set;}
+
+    }
+}
